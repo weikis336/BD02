1d9e5e0ad2827e189a5bdfd8c32b4e5a995acbb9
weikis336 <93678854+weikis336@users.noreply.github.com>
2022-12-02 21:39:01 +0100

    Update exercise.sql

--- exercise.sql
+++ exercise.sql
@@ -1,30 +1,29 @@
+--Crea la base de datos cine
 create Database cine
+--pasamos a usar la base de datos creada
 use cine
-Create table nombre film (
-                    film_id int(15) primary key,
-                    title varchar(50) not null,
-                    run_time int(3), 
+--Crea las tablas film actor y character con sus atributos
+CREATE TABLE film (
+					id INT(15)  AUTO_INCREMENT PRIMARY KEY,
+                    title varchar(50),
+					run_time int(3), 
                     genres varchar(20),
-                    ratting int(2) default 10, 
+                    ratting int(2), 
                     synopsis varchar(200),
                     directors varchar(100),
                     country varchar(100),
                     release_date date,
                     languaje varchar(25),
                     soundtrack varchar(20),
-                    screenwriters varchar(20),
-                  -- check (ratting<=10) comprueba que el valor introducido sea inferior a 10
-                  -- check (run_time<=300) con esto comprobamos la duracion de la pelicula
-
+                    screenwriters varchar(20)
 );
 create table actor (
                      actor_id int(16) primary key,
-                     fullname varchar(25) unique,
+                     fullname varchar(25),
                      birthdate date,
                      death_date date,
                      nationality varchar(30),
                      biography varchar(150)
-                     --check (date)
 
 );
 create table characer (
@@ -32,7 +31,6 @@ create table characer (
                         characer_id int(20) primary key,
                         character_fullname varchar(30),
                         char_description varchar(50),
-                        protagonist 
-                        oscar 
-
+                        protagonist varchar(15),
+                        oscar BOOL
 );
